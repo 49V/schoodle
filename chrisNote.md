bc49304c747c1f7725baf8f4fff8c435ba4e0718
Christina <christina.hsu.ca@gmail.com>
2019-02-01 00:53:56 +0000

    adding basic routing and sessionCookie

---
+++ chrisNote.md
@@ -0,0 +1,16 @@
+work journal Jan 31
+1. server.js file working - logging out the listening on port
+2. console.log out the linkages to database (tbu later)
+3. all user stories set up
+4. 11 digits random num urls
+5. nodemon working for now
+6. setup cookies
+
+
+
+to do 
+- how to extract data from front-end
+- data as part of the render page
+
+- supply the urls to all the other links    
+
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 "use strict";
 
-require('dotenv').config();
+// require('dotenv').config(); //need to uncomment
 
 const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
+// const ENV         = process.env.ENV || "development"; //need to uncomment
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
 
 const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
+// const knex        = require("knex")(knexConfig[ENV]); //need to uncomment
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+const cookieSession = require("cookie-session")
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -23,7 +24,7 @@ const usersRoutes = require("./routes/users");
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
-app.use(knexLogger(knex));
+// app.use(knexLogger(knex)); //need to uncomment
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -36,13 +37,104 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+// app.use("/api/users", usersRoutes(knex)); //need to uncomment
+
+//use cookies
+app.use(cookieSession({
+  name: 'session',
+  key: ['demo'],
+  maxAge: 24 * 60 * 60 * 1000
+}))
+
+//tentative event database
+const eventDB = {
+  randomUrl: {
+    pk: randomUrl,
+    title: req.body.title,         //event name
+    location: req.body.location,   //event location
+    descipt: req.body.description,
+    user: req.session.user_id     //organizers_id
+  }
+}
+
+//tentative responseDB
+const responseDB = {
+  randomUrl: {
+    name: req.body.name,            //response name
+    question1: req.body.question1,  //response1
+    question2: req.body.question2   //response2
+  }
+}
 
 // Home page
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("MainPage");
 });
+
+//create a new event with event name and description by an organizer, store a cookie
+app.post("/events", (req, res) => {
+  const randomUrl = generateRandomString();
+  eventDB[randomUrl] = {
+    pk: randomUrl,
+    title: req.body.title,         //event name
+    location: req.body.location,   //event location
+    descipt: req.body.description,  //event description
+    user: req.session.user_id     //organizers_id
+  }
+
+  res.redirect("/events/" + randomUrl);
+});
+
+//access a specific created/shareable url, all users are able to access this page once provided.
+app.get("/events/:id", (req, res) => {
+  res.render("responsePageHere");
+});
+
+//close the poll by the organizer
+app.post("/events/:id", (req, res) => {
+  //if the confirm button is clicked, all input will be closed
+  res.redirect("eventPageClose");
+});
+
+//modify the poll page by the organizer
+app.patch("/events/:id", (req, res) => {
+  
+  res.render("events");
+})
+
+//delete the poll page by the organizer
+app.delete("/events/:id", (req, res) => {
+  res.send("this page has been deleted by the orgaznier");
+});
+
+//access the response page by the attendees to view all responses/checking on final decision
+app.get("/events/:id/responses", (req, res) => {
+  res.render("responsePageHere");
+});
+
+//post information to the response page by the attendees
+app.post("/events/:id/responses", (req, res) => {
+  responseDB[randomUrl] = {
+    name: req.body.name,
+    question1: req.body.question1,
+    question2: req.body.question2
+  }
+  res.redirect("responseMainHere");
+});
+
+//modify information to the response specific to an attendee
+app.patch("/events/:id/responses/:id", (req, res) => {
+  res.redirect("responsePageHere");
+});
+
+//generate random strings(11 digits) for URLs
+function generateRandomString() {
+  return Math.random().toString(36).substring(2);
+}
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+
