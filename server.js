54feb4daf5af795f0c91643c1dfed4e3c8b158bc
Erin Toth <erintoth@gmail.com>
2019-02-03 23:06:00 +0000

    Add functionality for attendees to be able to create responses

--- server.js
+++ server.js
@@ -158,11 +158,25 @@ app.get("/events/:id", (req, res) => {
             eventLink: req.params.id  
           }
         return res.render('eventPageAttendee.ejs', templateVars);
-        console.log(rows);
     })
 
   }
 
+});
+
+app.post("/events/:id", (req, res) => {
+
+   const attendeeVote1 = (req.body.attendeeVote1) ? req.body.attendeeVote1 : 0;
+   const attendeeVote2 = (req.body.attendeeVote2) ? req.body.attendeeVote2 : 0;
+   const response = {
+       attendee_name: req.body.attendeeName,
+       attendee_email: req.body.attendeeEmail,
+       dates: [attendeeVote1, attendeeVote2],
+       events_id: req.params.id
+   }
+   dataHelpers.createResponse(response)
+
+    return res.redirect(`/events/${req.params.id}`);
 });
 
 
