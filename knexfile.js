308a80f20729bf222d61fc0eb774385d4b55f089
Erin Toth <erintoth@gmail.com>
2019-02-03 21:36:31 -0800

    heroku

--- knexfile.js
+++ knexfile.js
@@ -27,7 +27,8 @@ module.exports = {
         host     : process.env.DB_HOST,
         user     : process.env.DB_USER,
         password : process.env.DB_PASS,
-        database : process.env.DB_NAME,
+        database : postgres://usxaqtryqkuaph:6d4e6d5c4f9310e40f62f0e63a859a2f5c9bdd421094c1f92b8e3f31cb55593f@ec2-54-83-50-174.compute-1.amazonaws.com:5432/d1idqmj4hvroth
+        ,
         port     : process.env.DB_PORT,
         ssl      : process.env.DB_SSL
       },
