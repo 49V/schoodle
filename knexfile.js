2278235779d26657f633c8d91e6bba4e767da2c5
Erin Toth <erintoth@gmail.com>
2019-02-05 17:43:27 +0000

    Adding screenshots for README

--- knexfile.js
+++ knexfile.js
@@ -21,24 +21,17 @@ module.exports = {
     }
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-        host     : process.env.DB_HOST,
-        user     : process.env.DB_USER,
-        password : process.env.DB_PASS,
-        database : postgres://usxaqtryqkuaph:6d4e6d5c4f9310e40f62f0e63a859a2f5c9bdd421094c1f92b8e3f31cb55593f@ec2-54-83-50-174.compute-1.amazonaws.com:5432/d1idqmj4hvroth
-        ,
-        port     : process.env.DB_PORT,
-        ssl      : process.env.DB_SSL
-      },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'migrations'
-    }
-  }
+//   production: {
+//     client: 'postgresql',
+//     connection: "postgres://usxaqtryqkuaph:6d4e6d5c4f9310e40f62f0e63a859a2f5c9bdd421094c1f92b8e3f31cb55593f@ec2-54-83-50-174.compute-1.amazonaws.com:5432/d1idqmj4hvroth"
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//         directory: './db/migrations',
+//       tableName: 'migrations'
+//     }
+//   }
 
 };
